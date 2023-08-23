b713daa0f0bbc27d7b74e766935962e641634402
Pedro Araújo <pedroaraujo051100@gmail.com>
2023-08-22 22:46:56 -0300

    Getting temporary email

--- index.js
+++ index.js
@@ -2,4 +2,16 @@ const selenium = require('selenium-webdriver');
 
 const driver = new selenium.Builder().forBrowser('chrome').build();
 
-driver.get('https://www.google.com');
+var url = "";
+
+async function main() {
+    await driver.get('https://tuamaeaquelaursa.com')
+    await driver.findElement(selenium.By.xpath('//*[@id="header"]/div[2]/form/div[2]/button')).click();
+    let btn = await driver.findElement(selenium.By.xpath('/html/body/div/section/div/div'))
+    await driver.wait(selenium.until.elementIsVisible(btn), 5000);
+    await driver.getCurrentUrl().then(email => {
+        utl = email.split('').splice(29).join('')+"@tuamaeaquelaursa.com";
+    });
+}
+
+main()
